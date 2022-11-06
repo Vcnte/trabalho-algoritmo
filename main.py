b42d5fb3e556403818d7ef2874354350f69dcedc
Guilherme Vicente <gui.vcnt18@gmail.com>
2022-11-06 00:48:54 -0300

    trabalho de algoritmos

---
+++ main.py
@@ -0,0 +1,74 @@
+abrirdoc = input("Digite aqui o nome do arquivo: ")
+try:
+    localizardoc = open(abrirdoc)
+except FileNotFoundError:
+    print("Arquivo não encontrado")
+    exit()
+data = localizardoc.read().split()
+
+if localizardoc == None:
+    print("Não foi possível localizar/abrir o documento.")
+
+else:
+    print("1 - Consultar a quantidade de ocorrencias de uma determinada palavra\n"
+          "2 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem crescente de ocorrencia\n"
+          "3 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem decrescente de ocorrencia\n"
+          "4 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem alfabetica\n"
+          "5 - Listar todas as palavras que constam no arquivo e que iniciam por um caractere informado pelo usuario, bem como a quantidade de vezes que essas palavras ocorrem, em ordem crescente de ocorrencia\n"
+          "6 - Encerrar o programa")
+    while True:
+        opcaomenu = input("Digite a opção desejada: ")
+        if opcaomenu == "1":
+            palavraencontrada = input("Digite aqui a palavra a ser encontrada: ")
+            if palavraencontrada in data:
+                numerodepalavrasiguais = data.count(palavraencontrada)
+                print(numerodepalavrasiguais)
+            else:
+                print("Palavra não encontrada, tente novamente.")
+
+        elif opcaomenu == "2":
+            dicionario = {}
+            for palavra in data:
+                if palavra in dicionario:
+                    dicionario[palavra] += 1
+                else:
+                    dicionario[palavra] = 1
+            dicionario_ordenado = sorted(dicionario.items(), key=lambda x: x[1], reverse=False)
+
+            print(dicionario_ordenado)
+        elif opcaomenu == "3":
+            dicionario = {}
+            for palavra in data:
+                if palavra in dicionario:
+                    dicionario[palavra] += 1
+                else:
+                    dicionario[palavra] = 1
+            dicionario_ordenado = sorted(dicionario.items(), key=lambda x: x[1], reverse=True)
+
+            print(dicionario_ordenado)
+        elif opcaomenu == "4":
+            dicionario = {}
+            for palavra in data:
+                if palavra in dicionario:
+                    dicionario[palavra] += 1
+                else:
+                    dicionario[palavra] = 1
+            dicionario_ordenado = sorted(dicionario.keys())
+            print(dicionario_ordenado)
+        elif opcaomenu == "5":
+            caractere = input("Digite aqui o caractere desejado: ")
+            dicionario = {}
+            for palavra in data:
+                if caractere == palavra[0]:
+                    if palavra in dicionario:
+                        dicionario[palavra] += 1
+                    else:
+                        dicionario[palavra] = 1
+            dicionario_ordenado = sorted(dicionario.items(), key=lambda x: x[1], reverse=False)
+            print(dicionario_ordenado)
+
+        elif opcaomenu == "6":
+            print("Finalizando o programa")
+            raise SystemExit
+        else:
+            print("ERRO: Digite uma opção válida.")
